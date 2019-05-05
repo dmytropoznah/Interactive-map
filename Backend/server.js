c42f83f587ec4645f93ca2addc0f51d9e65e5182
dmytropoznah <dmytropoznah>
2019-05-05 19:09:54 +0300

    Added graph. Data is getting from database

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,86 @@
 const http = require("http");
 const mysql = require('mysql');
-const express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const app = express();
+//cors
+var cors = require('cors')
 
-http.createServer(function(request, response) {
 
-	var db = mysql.createConnection({
-	    host: "localhost",
-		port: 3306,
-  		user: "root",
- 	    password: "123456",
- 	    database: "db",
-	});
+//routing
+var apps = express();
+var router = express.Router();
 
+apps.use(cors());
+apps.use(bodyParser.json());
+
+apps.use('/map', router);
+router.get('/crops', function(req, resp) {
+
+var db = mysql.createConnection({
+      host: "localhost",
+    port: 3306,
+      user: "root",
+      password: "123456",
+      database: "db",
+  });
+  
 db.connect(function(err) {
   if (err) throw err;
+  console.log('param ', req.query.name);
+  db.query("SELECT crop_area, name_kultura, year FROM kultura WHERE region = 'Запорізька область' and name_kultura = '" + req.query.name + "'", function (err, results, rows) {
+    if (err) throw err;
+    console.log(results);
+
+    resp.send(results); 
+  });
+});
+ 
+});
+
+router.get('/names', function(req, resp) {
+
+var db = mysql.createConnection({
+      host: "localhost",
+    port: 3306,
+      user: "root",
+      password: "123456",
+      database: "db",
+  });
+  
+db.connect(function(err) {
+  if (err) throw err;
+  db.query("SELECT crop_area, name_kultura, year FROM kultura WHERE region = 'Запорізька область' GROUP BY name_kultura", function (err, results, rows) {
+    if (err) throw err;
+    console.log(results);
+
+    resp.send(results);
+  });
+});
+  
+});
+
+
+
+
+	
+
+/*db.connect(function(err) {
+  if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE bam (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE ссссm (name VARCHAR(255), address VARCHAR(255))";
   
   db.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
-});
+});*/
 
 
-response.end("listen3000...");
-}).listen(3000);
+
+apps.listen(8080, function() {
+  console.log("Ready  . . . ");
+ });
 
 
 
